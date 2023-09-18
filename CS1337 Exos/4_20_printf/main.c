53b36345de5ecddf9136c026d08a0ca4554519ce
Evan Wright <124637211+emw8105@users.noreply.github.com>
2023-09-18 14:00:50 -0500

    Last Exos
    Exos are small daily programs that are used for demonstration purposes of various concepts in C++

---
+++ CS1337 Exos/4_20_printf/main.c
@@ -0,0 +1,84 @@
+// 4_20_printf
+/*
+printf has this format: printf(format-control-string, zero or more arguments)
+format-control-string is always in double quotes
+format-control-string has zero or more conversion specifications and zero
+or more character literals
+%e and %E will print floating point in scientific notation
+%f will print floating point data with a decimal point, with 6 digits
+after the point by default
+%g and %G will print floating point data in scientific notation if the
+value is very large or very small, fixed point otherwise
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n"); // format control string has zero
+    // conversion specification and 13 character literals
+
+    // Print integers
+    int num1 = 10;
+
+    // %d prints in decimal
+    printf("num1 with the d specifier (decimal) is %d, 5 in decimal is %d\n",
+           num1, 5);
+
+    // %i also prints in decimal
+    printf("num1 with the i specifier is %i, 5 in decimal is %d\n",
+           num1, 5);
+
+    // %u prints in unsigned
+    printf("num1 with the u specifier is %u, 5 in decimal is %d\n",
+           num1, 5);
+
+    // %o prints in base 8 (octal)
+
+    printf("num1 with the o specifier is %o, 5 in decimal is %d\n",
+           num1, 5);
+
+    // %x prints in base 16 (hexadecimal)
+    printf("num1 with the x specifier is %x, 5 in decimal is %d\n",
+           num1, 5);
+
+    // %X prints in base 16 (hexadecimal)
+    printf("num1 with the X specifier is %X, 5 in decimal is %d\n",
+           num1, 5);
+
+    // Floating point
+    printf("\nFloating point\n");
+    printf("1234567.89 with the %%e specifier is %e\n", 1234567.89);
+    printf("1234567.89 with the %%f specifier is %f\n", 1234567.89);
+    printf("1234567.89 with the %%g specifier is %g\n", 1234567.89);
+
+    printf("0.000000123456789 with the %%e specifier is %e\n", 0.000000123456789);
+    printf("0.000000123456789 with the %%f specifier is %f\n", 0.000000123456789);
+    printf("0.000000123456789 with the %%g specifier is %g\n", 0.000000123456789);
+
+    printf("123.456 with the with the %%e specifier is %e\n", 123.456);
+    printf("123.456 with the with the %%f specifier is %f\n", 123.456);
+    printf("123.456 with the with the %%g specifier is %g\n", 123.456);
+
+    //Precision
+    printf("\nPrecision\n");
+    printf("0.000000123456789 with the %%15.13f specifier is %15.13f", 0.000000123456789);
+    printf("0.000000123456789 with the %%15.6g specifier is %15.6g", 0.000000123456789);
+
+    // Chars and strings
+    printf("\nChars and strings\n");
+    printf("My char is %c\n", 'd');
+    char c = 'a';
+    printf("My char is %c\n", c);
+    char myString[] = "Hi there";
+    printf("%s\n", myString);
+
+    // Pointer
+    int num = 5;
+    printf("\nPointers\n");
+    printf("Address of num is %p\n", (void *) &num); // Type cast to void*
+
+    return 0;
+}
---
+++ CS1337 Exos/4_25_scanf/main.c
@@ -0,0 +1,84 @@
+// 4_25_scanf
+
+/*
+scanf format is scanf(format-control-string, one or more arguments)
+Arguments are referred to by their addresses (use & operator)
+Many conversion specifiers are common with printf
+With %i, the user can enter a value in decimal, octal, or hexadecimal
+%f, %e, %g are interchangeable for floating points
+To read a string, the argument is the array name (no need for &)
+The field width can be used to limit the number of chars read
+%n will return the number of characters typed so far in the current scanf
+The same left over character issue as in C++ exists: need to flush
+the buffer before a scanf("%c", ...)
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1, num;
+    unsigned num2, num3; // %o and %x must be positive
+    float flt1, flt2, flt3, flt;
+    double doub;
+
+    // Read in decimal, hex and octal
+    printf("Enter 3 numbers in decimal, hexadecimal and octal: ");
+    scanf("%d%x%o", &num1, &num2, &num3);
+    printf("Numbers in decimal are %d, %d, %d\n", num1, num2, num3);
+
+    // %i specifier
+    printf("Enter a decimal, octal (prepend 0), or a hex number (prepend 0x): ");
+    scanf("%i", &num);
+    printf("Value entered is %d\n", num);
+
+    // Floating point: %e, %f, and %g are interchangeable
+    printf("Enter 3 floating point numbers: ");
+    scanf("%e%f%g", &flt1, &flt2, &flt3);
+    printf("flt1 = %f, flt2 = %f, flt3 = %f\n", flt1, flt2, flt3);
+
+    // Mix various types
+    printf("Enter an int, a float and a double: ");
+    scanf("%d%f%lf", &num, &flt, &doub); // Use %f for float, %if for double
+    printf("num = %d, flt = %f, doub = %lf\n", num, flt, doub);
+
+    // Character and string
+    char c;
+    printf("Enter a character: ");
+    scanf("%c", &c); // Flush the buffer of the leftover new line
+    scanf("%c", &c);
+    printf("Char entered is %c\n");
+    char myStr[10];
+    printf("Enter a string: ");
+    scanf("%s", myStr); // myStr is the address of the array, no need for &
+    printf("String entered is %s\n", myStr);
+
+    // Use fieldwidth to limit the number of characters written into the array
+    // to avoid overflowing the array
+    // Use %n specifier to know how many characters have been inputted so far
+    // in the current scan f
+    int count1, count2;
+    printf("Enter a string, max 9 characters and an int: ");
+    scanf("%9s%n%d%n", myStr, &count1, &num, &count2);
+    printf("String entered is %s, int entered is %d\n", myStr, num);
+    printf("%d chars have been entered after the string, and %d chars entered after the int\n",
+           count1, count2);
+
+    // Scanset
+    char str[10];
+    printf("Scanset is [aeiou], enter string: ");
+    scanf("%c", &c); // Flush the buffer
+    scanf("%[aeiou]", str);
+    printf("The input was \"%s\"", str);
+
+    // Suppression character
+    int m, d, y;
+    printf("Enter a date in the form mm-dd-yyyy: ");
+    while((getchar()) != '\n'); // Flush the buffer
+    scanf("%c", &c); // Flush the buffer
+    scanf("%d%*c%d%*c%d", &m, &d, &y);
+    printf("d = %d, m = %d, y = %d\n", m, d, y);
+
+    return 0;
+}
